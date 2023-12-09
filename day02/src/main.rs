4c1bb903ac577dbe20f2a853d3d26908c4dbf3ff
Gereon V <72784429+GereonV@users.noreply.github.com>
2023-12-10 00:32:43 +0100

    Review day 1 and 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -4,45 +4,39 @@ use std::collections::HashMap;
  * max_cubes takes numbers as (red, green, blue)
  */
 fn part1(file_content: &str, max_cubes: (u32, u32, u32)) -> u32 {
-    return file_content
+    file_content
         .lines()
         .enumerate()
-        .map(|(i, line)| {
+        .filter_map(|(i, line)| {
             for game in line.split(":").last().unwrap().split(";") {
                 for pull in game.split(",").map(|s| s.trim().to_lowercase()) {
                     let (amount, color) = pull.split_at(pull.find(" ").unwrap());
                     let amount: u32 = amount.parse().unwrap();
                     let color = color.trim();
 
-                    let too_many = match color {
-                        "red" => amount > max_cubes.0,
-                        "green" => amount > max_cubes.1,
-                        "blue" => amount > max_cubes.2,
+                    let color_amt = match color {
+                        "red" => max_cubes.0,
+                        "green" => max_cubes.1,
+                        "blue" => max_cubes.2,
                         _ => panic!("Number expected"),
                     };
 
-                    if too_many {
-                        return 0;
+                    if amount > color_amt {
+                        return None;
                     }
                 }
             }
 
-            return (i + 1) as u32;
+            Some((i + 1) as u32)
         })
-        .sum();
+        .sum()
 }
 
-/**
- * max_cubes takes numbers as (red, green, blue)
- */
 fn part2(file_content: &str) -> u32 {
-    return file_content
+    let colormap: HashMap<&str, usize> = HashMap::from([("red", 0), ("green", 1), ("blue", 2)]);
+    file_content
         .lines()
         .map(|line| {
-            let colormap: HashMap<&str, usize> =
-                HashMap::from([("red", 0), ("green", 1), ("blue", 2)]);
-
-            let mut sum: u32 = 1;
             let mut minimum_amount: [u32; 3] = [0, 0, 0];
 
             for game in line.split(":").last().unwrap().split(";") {
@@ -50,19 +44,16 @@ fn part2(file_content: &str) -> u32 {
                     let (amount, color) = pull.split_at(pull.find(" ").unwrap());
 
                     let amount: u32 = amount.parse().unwrap();
-                    let index = colormap.get(color.trim()).unwrap().clone();
+                    let index = *colormap.get(color.trim()).unwrap();
 
-                    minimum_amount[index] = std::cmp::max(minimum_amount[index], amount)
+                    let amt_ref = &mut minimum_amount[index];
+                    *amt_ref = std::cmp::max(*amt_ref, amount)
                 }
             }
 
-            minimum_amount.iter().for_each(|v| {
-                sum *= v;
-            });
-
-            return sum;
+            minimum_amount.iter().fold(1, |x, v| x * v)
         })
-        .sum();
+        .sum()
 }
 
 fn main() {
